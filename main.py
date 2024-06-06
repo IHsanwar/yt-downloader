46927c5e1116abcba057ba61156ec4ddd7abb775
IhsanWar810 <ihsanwar77@gmail.com>
2024-06-06 19:28:37 +0700

    pesan komit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template, send_file
+from pytube import YouTube
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/download', methods=['POST'])
+def download():
+    ytlink = request.form['ytlink']
+    ytdownload = YouTube(ytlink)
+    stream = ytdownload.streams.get_highest_resolution()
+    file_path = stream.download()
+
+    return send_file(file_path, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
